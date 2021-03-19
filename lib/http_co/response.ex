c873c5c75f20b33f8f157e3f2e21ee5db8f63f2e
Simon Thörnqvist <simon.thornqvist@gmail.com>
2021-03-19 20:55:01 +0100

    init response mod
    await fun for awaiting Mint messages

---
+++ lib/http_co/response.ex
@@ -0,0 +1,56 @@
+defmodule HTTPCo.Response do
+  @type t :: %__MODULE__{}
+  defstruct [
+    :ref,
+    :conn,
+    :errors,
+    :status_code,
+    :content_length,
+    :headers,
+    :body
+  ]
+
+  @spec new(Keyword.t()) :: t()
+  def new(opts) do
+    struct(__MODULE__, opts)
+  end
+
+  @spec await(t()) :: t()
+  def await(%__MODULE__{conn: conn} = res) do
+    receive do
+      message ->
+        {:ok, conn, responses} = Mint.HTTP.stream(conn, message)
+
+        case handle_messages(%{res | conn: conn}, responses) do
+          {:cont, res} -> await(res)
+          {:halt, res} -> res
+        end
+    end
+  end
+
+  defp handle_messages(%__MODULE__{} = res, responses) do
+    Enum.reduce(responses, {:cont, res}, &handle_message/2)
+  end
+
+  defp handle_message(response, {_, %__MODULE__{ref: ref} = res}) do
+    case response do
+      {:status, ^ref, status_code} -> {:cont, %{res | status_code: status_code}}
+      {:headers, ^ref, headers} -> {:cont, %{res | headers: headers}}
+      {:data, ^ref, data} -> {:cont, %{res | body: data}}
+      {:done, ^ref} -> {:halt, res}
+    end
+  end
+
+  defp handle_message(
+         {:status, request_ref, status_code},
+         {_, %__MODULE__{ref: ref} = res}
+       )
+       when request_ref == ref do
+    {:cont, %{res | status_code: status_code}}
+  end
+
+  @spec with_error(t(), term()) :: t()
+  def with_error(res, _reason) do
+    res
+  end
+end
