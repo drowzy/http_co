e2956f1b2e71d140c4dc315b40aa4273cbcc1af2
Simon Thörnqvist <simon.thornqvist@gmail.com>
2021-03-19 15:00:52 +0100

    add into_conn func for picking fields in use for a mint conn

--- lib/http_co/request.ex
+++ lib/http_co/request.ex
@@ -16,24 +16,24 @@ defmodule HTTPCo.Request do
   @type url :: String.t() | Uri.t()
   @type header_value :: {String.t(), String.t()}
 
-  @spec get(url(), list()) :: t()
-  def get(url, headers \\ []) do
-    build(:get, url, headers, [])
+  @spec get(url()) :: t()
+  def get(url) do
+    build(:get, url, [], [])
   end
 
-  @spec post(url(), list()) :: t()
-  def post(url, headers \\ []) do
-    build(:post, url, headers, [])
+  @spec post(url()) :: t()
+  def post(url) do
+    build(:post, url, [], [])
   end
 
-  @spec put(url(), list()) :: t()
-  def put(url, headers \\ []) do
-    build(:put, url, headers, [])
+  @spec put(url()) :: t()
+  def put(url) do
+    build(:put, url, [], [])
   end
 
-  @spec delete(url(), list()) :: t()
-  def delete(url, headers \\ []) do
-    build(:delete, url, headers, [])
+  @spec delete(url()) :: t()
+  def delete(url) do
+    build(:delete, url, [], [])
   end
 
   @spec build(http_method(), url(), list(), iodata()) :: t()
@@ -55,6 +55,22 @@ defmodule HTTPCo.Request do
   @spec request_path(t()) :: String.t()
   def request_path(%__MODULE__{path: path, query: query}) do
     path <> qs(query)
+  end
+
+  @spec into_conn(t()) :: {term(), term()}
+  def into_conn(%__MODULE__{
+        method: method,
+        path: path,
+        host: host,
+        port: port,
+        scheme: scheme
+      }) do
+    method = method_to_string(method)
+
+    case scheme do
+      :unix -> {method, {:http, {:local, path}, 0}}
+      scheme -> {method, {scheme, host, port}}
+    end
   end
 
   @spec set_header(t(), header_value()) :: t()
@@ -81,16 +97,23 @@ defmodule HTTPCo.Request do
   defp parse_url(%URI{} = uri) do
     path = uri.path || "/"
     query = URI.decode_query(uri.query || "")
-    scheme = parse_scheme(uri.scheme)
+    scheme = parse_scheme(uri.scheme, uri)
 
     {scheme, uri.host, uri.port, path, query}
   end
 
-  defp parse_scheme("https"), do: :https
-  defp parse_scheme("http"), do: :http
-  defp parse_scheme("http+unix"), do: :unix
-  defp parse_scheme("unix"), do: :unix
+  defp parse_scheme("https", _), do: :https
+  defp parse_scheme("http", _), do: :http
+  defp parse_scheme("http+unix", _), do: :unix
+  defp parse_scheme("unix", _), do: :unix
+
+  defp parse_scheme(nil, uri),
+    do: raise(ArgumentError, "scheme is required for url: #{URI.to_string(uri)}")
+
+  defp parse_scheme(scheme, uri),
+    do: raise(ArgumentError, "invalid scheme \"#{scheme}\" for url: #{URI.to_string(uri)}")
 
   defp qs(query) when map_size(query) == 0, do: ""
   defp qs(query), do: "?" <> URI.encode_query(query)
+  defp method_to_string(method), do: method |> Atom.to_string() |> String.upcase()
 end
--- test/http_co/request_test.exs
+++ test/http_co/request_test.exs
@@ -1,11 +1,12 @@
 defmodule HTTPCo.RequestTest do
   use ExUnit.Case
-  doctest HTTPCo.Request
+  alias HTTPCo.Request
+
+  doctest Request
 
   describe "constructors" do
     test "build/4" do
-      assert %HTTPCo.Request{} =
-               req = HTTPCo.Request.build(:get, "http://localhost/path?key=value", [], [])
+      assert %Request{} = req = Request.build(:get, "http://localhost/path?key=value", [], [])
 
       assert req.scheme == :http
       assert req.path == "/path"
@@ -16,27 +17,27 @@ defmodule HTTPCo.RequestTest do
     end
 
     test "post/1 returns a request struct with method :post" do
-      assert %HTTPCo.Request{method: :post} = HTTPCo.Request.post("http://localhost")
+      assert %Request{method: :post} = Request.post("http://localhost")
     end
 
     test "put/1 returns a request struct with method :put" do
-      assert %HTTPCo.Request{method: :put} = HTTPCo.Request.put("http://localhost")
+      assert %Request{method: :put} = Request.put("http://localhost")
     end
 
     test "get/1 returns a request struct with method :get" do
-      assert %HTTPCo.Request{method: :get} = HTTPCo.Request.get("http://localhost")
+      assert %Request{method: :get} = Request.get("http://localhost")
     end
 
     test "delete/1 returns a request struct with method :delete" do
-      assert %HTTPCo.Request{method: :delete} = HTTPCo.Request.delete("http://localhost")
+      assert %Request{method: :delete} = Request.delete("http://localhost")
     end
   end
 
   describe "set_header/2" do
     test "adds the header pair to headers" do
       header = {"Content-type", "application/json"}
-      req = %HTTPCo.Request{query: %{}}
-      assert %HTTPCo.Request{headers: headers} = HTTPCo.Request.set_header(req, header)
+      req = %Request{query: %{}}
+      assert %Request{headers: headers} = Request.set_header(req, header)
 
       assert hd(headers) == header
     end
@@ -44,30 +45,56 @@ defmodule HTTPCo.RequestTest do
 
   describe "request_path/1" do
     test "returns `path` only if there's no query params" do
-      req = %HTTPCo.Request{query: %{}, path: "/foo"}
-      assert "/foo" == HTTPCo.Request.request_path(req)
+      req = %Request{query: %{}, path: "/foo"}
+      assert "/foo" == Request.request_path(req)
     end
 
     test "returns `<path>?<query0>&<query..n>` if query params are set" do
-      req = %HTTPCo.Request{query: %{"key" => "value", "key2" => "value2"}, path: "/foo"}
-      assert "/foo?key=value&key2=value2" == HTTPCo.Request.request_path(req)
+      req = %Request{query: %{"key" => "value", "key2" => "value2"}, path: "/foo"}
+      assert "/foo?key=value&key2=value2" == Request.request_path(req)
     end
   end
 
   describe "setters" do
     test "adds the header pair to headers" do
       header = {"Content-type", "application/json"}
-      req = %HTTPCo.Request{}
-      assert %HTTPCo.Request{headers: headers} = HTTPCo.Request.set_header(req, header)
+      req = %Request{}
+      assert %Request{headers: headers} = Request.set_header(req, header)
 
       assert hd(headers) == header
     end
 
-    test "adds query kv-pari to query" do
-      req = %HTTPCo.Request{query: %{}}
-      assert %HTTPCo.Request{query: query} = HTTPCo.Request.set_query_item(req, {"key", "value"})
+    test "adds query kv pair to query" do
+      req = %Request{query: %{}}
+      assert %Request{query: query} = Request.set_query_item(req, {"key", "value"})
 
       assert Map.get(query, "key") == "value"
+    end
+  end
+
+  describe "into_conn/1" do
+    test "for unix socket" do
+      socket_path = "/var/run/docker.sock"
+      url = "unix://#{socket_path}"
+      req = Request.get(url)
+
+      assert {"GET", {:http, {:local, ^socket_path}, 0}} = Request.into_conn(req)
+    end
+
+    test "for unix socket + http scheme" do
+      socket_path = "/var/run/docker.sock"
+      url = "http+unix://#{socket_path}"
+      req = Request.get(url)
+
+      assert {"GET", {:http, {:local, ^socket_path}, 0}} = Request.into_conn(req)
+    end
+
+    test "for http" do
+      host = "httpbin.org"
+      url = "http://#{host}"
+      req = Request.get(url)
+
+      assert {"GET", {:http, ^host, 80}} = Request.into_conn(req)
     end
   end
 end
